52a974e38c5c7dbd5739792891363de9687729ac
Pedro Arthur <pedroarthur1999@gmail.com>
2023-01-07 00:37:41 -0300

    New file

---
+++ arrowFunctions.js
@@ -0,0 +1,21 @@
+//Utilizando funções de seta
+
+//Diferença de sintaxe
+
+function teste(testando) {
+    return testando
+}
+
+//Arrow function (as 2 funcionam)
+
+(testando) => {
+    return testando
+}
+
+//se a arrow function tiver apenas um parametro, pode se omitir os parenteses
+
+testando => {return testando}
+
+//ao remover as chaves da arrow function, vc pode obter um retorno implicito
+
+testando => testando
---
+++ removendoElementosDeUmArray.js
@@ -0,0 +1,20 @@
+//Removendo elementos de um array
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+//Removendo o ultimo elemento de um array 
+
+numeros.pop()
+
+console.log(numeros);
+
+//Removendo o primeiro elemento de um array
+
+numeros.shift()
+
+console.log(numeros);
+
+//Removendo no meio de um array
+
+numeros.splice(2,2)
+console.log(numeros);
